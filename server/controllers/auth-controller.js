4e41744f4d31ab7444a9c8276f99f1e4bc69ef24
mohitDora <doramohitkumar@gmail.com>
2024-05-04 16:06:51 +0530

    c-3

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -1,40 +1,49 @@
 const User = require("../models/user-model");
-const cloudinary = require("../utils/cloudinary")
+const cloudinary = require("../utils/cloudinary");
 
 const register = async (req, res) => {
-    try {
-
-        const { name, email, gender, profilePicture, uid } = req.body;
-        let result=profilePicture
-        if(profilePicture){
-            result = await cloudinary.uploader.upload(profilePicture,
-                { folder: "test", width: 300, height: 300, crop: "fill" });
-            console.log(result)
-        }
-        
-        // const result1 = await cloudinary.uploader.upload(coverPicture,
-        //     { folder: "test", width: 300, height: 300, crop: "fill" });
-        // console.log(result)
-
-        const user = await User.create({ name, email, gender, profilePicture:result.secure_url || profilePicture, uid })
-        console.log(user)
-        res.status(200).json({ user })
-    } catch (error) {
-        res.status(501).json("server error")
+  try {
+    const { name, email, gender, profilePicture, uid } = req.body;
+    let result = profilePicture;
+    if (profilePicture) {
+      try {
+        result = await cloudinary.uploader.upload(profilePicture, {
+          folder: "test",
+          width: 300,
+          height: 300,
+          crop: "fill",
+        });
+        console.log(result);
+      } catch (uploadError) {
+        console.error("Error uploading profile picture:", uploadError);
+        // Handle the error (optional)
+      }
     }
-}
+
+    const user = await User.create({
+      name,
+      email,
+      gender,
+      profilePicture: result.secure_url || profilePicture,
+      uid,
+    });
+    console.log(user);
+    res.status(200).json({ user });
+  } catch (error) {
+    res.status(501).json("server error");
+  }
+};
 
 const user = async (req, res) => {
-    try {
-        console.log(req.params)
-        const { uid } = req.params
-        const isPresent = await User.findOne({ uid });
+  try {
+    console.log(req.params);
+    const { uid } = req.params;
+    const isPresent = await User.findOne({ uid });
 
-        res.status(200).json(isPresent)
-    } catch (error) {
-        console.log(error)
-    }
+    res.status(200).json(isPresent);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-}
-
-module.exports = { register, user }
+module.exports = { register, user };
